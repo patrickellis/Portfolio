3fad6e46ce112dc2d74079f888b1542359386d89
Patrick Ellis <patrick.ellis.w@gmail.com>
2021-03-28 12:55:54 +0100

    updated links with new javascript include file, added jane street project page

---
+++ js/project_click_events.js
@@ -0,0 +1,10 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script>
+$("#first").click(function(){
+  menu_relocate("../all/project");
+});
+$("#second").click(function(){
+  menu_relocate("../all/project");
+});
+</script>
