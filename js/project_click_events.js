27a68c7d7ff42ef8f645d014a17acc87ce774615
Patrick Ellis <patrick.ellis.w@gmail.com>
2021-03-28 13:02:39 +0100

    script tags not needed

--- js/project_click_events.js
+++ js/project_click_events.js
@@ -1,10 +1,7 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<script>
 $("#first").click(function(){
   menu_relocate("../all/project");
 });
 $("#second").click(function(){
   menu_relocate("../all/project");
 });
-</script>
